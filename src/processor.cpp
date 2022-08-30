790e8d8c3b138af9f263f5a2be162b093570d826
AbhilashJoshi25 <107572032+AbhilashJoshi25@users.noreply.github.com>
2022-08-30 09:24:57 -0700

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,20 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <iostream>
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization(){
+    float iln = LinuxParser::IdleJiffies();
+    float n_iln = LinuxParser::ActiveJiffies();
+    //float tln = LinuxParser::Jiffies();
+    float temp = iln + n_iln;
+    float tl = temp - tlp;
+    float idl = iln - ilp;
+
+    //store values
+    tlp = temp;
+    ilp = iln;
+
+    //return percentage utilization
+    return (tl - idl)/tl;
+}
