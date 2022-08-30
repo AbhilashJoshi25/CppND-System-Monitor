4de1b4e48c21ca628b16102088946b5f07dc9147
AbhilashJoshi25 <107572032+AbhilashJoshi25@users.noreply.github.com>
2022-08-30 09:25:41 -0700

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -1,41 +1,20 @@
-#include <unistd.h>
-#include <cstddef>
-#include <set>
-#include <string>
-#include <vector>
-
-#include "process.h"
 #include "processor.h"
-#include "system.h"
+#include "linux_parser.h"
+#include <iostream>
 
-using std::set;
-using std::size_t;
-using std::string;
-using std::vector;
-/*You need to complete the mentioned TODOs in order to satisfy the rubric criteria "The student will be able to extract and display basic data about the system."
+// TODO: Return the aggregate CPU utilization
+float Processor::Utilization(){
+    float iln = LinuxParser::IdleJiffies();
+    float n_iln = LinuxParser::ActiveJiffies();
+    //float tln = LinuxParser::Jiffies();
+    float temp = iln + n_iln;
+    float tl = temp - tlp;
+    float idl = iln - ilp;
 
-You need to properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
+    //store values
+    tlp = temp;
+    ilp = iln;
 
-// TODO: Return the system's CPU
-Processor& System::Cpu() { return cpu_; }
-
-// TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
-
-// TODO: Return the system's kernel identifier (string)
-std::string System::Kernel() { return string(); }
-
-// TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
-
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
-
-// TODO: Return the number of processes actively running on the system
-int System::RunningProcesses() { return 0; }
-
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
-
-// TODO: Return the number of seconds since the system started running
-long int System::UpTime() { return 0; }
+    //return percentage utilization
+    return (tl - idl)/tl;
+}
