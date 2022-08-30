8cba2a91852af7f52669cf4079e09b942cb54f4b
AbhilashJoshi25 <107572032+AbhilashJoshi25@users.noreply.github.com>
2022-08-30 09:29:32 -0700

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -1,20 +1,71 @@
+#include <unistd.h>
+#include <cstddef>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "process.h"
 #include "processor.h"
+#include "system.h"
 #include "linux_parser.h"
-#include <iostream>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization(){
-    float iln = LinuxParser::IdleJiffies();
-    float n_iln = LinuxParser::ActiveJiffies();
-    //float tln = LinuxParser::Jiffies();
-    float temp = iln + n_iln;
-    float tl = temp - tlp;
-    float idl = iln - ilp;
+using std::set;
+using std::size_t;
+using std::string;
+using std::vector;
+/*You need to complete the mentioned TODOs in order to satisfy the rubric criteria "The student will be able to extract and display basic data about the system."
 
-    //store values
-    tlp = temp;
-    ilp = iln;
+You need to properly format the uptime. Refer to the comments mentioned in format. cpp for formatting the uptime.*/
 
-    //return percentage utilization
-    return (tl - idl)/tl;
+// TODO: Return the system's CPU
+Processor& System::Cpu(){
+    return cpu_;
+}
+
+// TODO: Return a container composed of the system's processes
+vector<Process>& System::Processes(){
+    processes_.clear();
+    vector<int>line_of_pids = LinuxParser::Pids();
+    //processes_ = {};
+    // int i = 0;
+    // while(i < line_of_pids.size()){
+    //     processes_.emplace_back(i);
+    //     i++;
+    // }
+    for (auto i : line_of_pids){
+        Process ps(i);
+        processes_.push_back(ps);
+    }
+    std::sort(processes_.begin(), processes_.end());
+    return processes_;
+}
+
+// TODO: Return the system's kernel identifier (string)
+std::string System::Kernel(){
+    return LinuxParser::Kernel();
+}
+
+// TODO: Return the system's memory utilization
+float System::MemoryUtilization(){
+    return LinuxParser::MemoryUtilization();
+}
+
+// TODO: Return the operating system name
+std::string System::OperatingSystem(){
+    return LinuxParser::OperatingSystem();
+}
+
+// TODO: Return the number of processes actively running on the system
+int System::RunningProcesses(){
+    return LinuxParser::RunningProcesses();
+}
+
+// TODO: Return the total number of processes on the system
+int System::TotalProcesses(){
+    return LinuxParser::TotalProcesses();
+}
+
+// TODO: Return the number of seconds since the system started running
+long int System::UpTime(){
+    return LinuxParser::UpTime();
 }
